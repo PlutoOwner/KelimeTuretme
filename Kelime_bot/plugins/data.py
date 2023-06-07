dc8dc111cd5179eb5b9a565283a532a3054f227b
PlutoOwner <123556006+PlutoOwner@users.noreply.github.com>
2023-06-07 22:41:22 +0300

    Update data.py

--- Kelime_bot/plugins/data.py
+++ Kelime_bot/plugins/data.py
@@ -1,8 +1,8 @@
-from main import oyun, rating
+from main import oyun, rating, OWNER_ID
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("data") & filters.user("PlutoOwner")) 
+@Client.on_message(filters.command("data") & filters.user("OWNER_ID")) 
 async def data(c:Client, m):
     await m.reply(oyun)
     await m.reply(rating)
